9e437f7c4d617c18936517dc3b53129be5a4332d
Yaman JAIOUCH <yjaiouch@gmail.com>
2014-07-08 21:32:13 +0200

    Comment ROKRadio header

--- RadioKit/ROKRadio.h
+++ RadioKit/ROKRadio.h
@@ -23,14 +23,29 @@ typedef NS_ENUM(NSUInteger, ROKRadioTrackOrder)
 
 @protocol ROKRequestParameter, ROKTrack;
 
+/**
+ *  `ROKRadio` protocol conforms to `ROKRequestParameter` making objects conforming `ROKRadio
+ *  suitable to perform request
+ *  The main difference reside on the track order, locating where is the most recent played track 
+ *  in the response (top or bottom).
+ *  This parameter will vary from a radio to an other.
+ */
 @protocol ROKRadio <ROKRequestParameter>
 
+// Describes where the most recent track is locating in the response (first one or last one)
 @property (assign, nonatomic) ROKRadioTrackOrder trackOrder;
 
 @end
 
+
+/**
+ *  `ROKRadio` is a basic object type used to describe a radio entity.
+ *  `ROKRadio` conforms to `ROKRadio` protocol which conforms itself to 'ROKRequestParameter` protocol
+ *  making these objects suitable to perform request.
+ */
 @interface ROKRadio : NSObject <ROKRadio>
 
+// See ROKRequest.h for explanations on the following properties
 @property (copy, nonatomic) NSString *requestURL;
 @property (copy, nonatomic) NSString *titleKeyPath;
 @property (copy, nonatomic) NSString *artistKeyPath;
@@ -46,19 +61,36 @@ typedef NS_ENUM(NSUInteger, ROKRadioTrackOrder)
 
 @end
 
+/**
+ *  The following methods are helper methods used to create `ROKRadio` request from a NSDictionary.
+ *  These methods used Key-Value Coding to create objects so dictionary's keys name should have the same name of the above properties.
+ *  These methods could be used to bind `ROKRadio` objects from a large set of radios data (in plist for example)
+ */
 @interface ROKRadio (ROKKeyValue)
 
 + (instancetype)radioWithDictionary:(NSDictionary *)dictionary;
 
 @end
 
+/**
+ *  The following methods are the best way to easily retrieve last played tracks
+ *  Results type are either NSDictionary objects type or custom objects type if you have set the `trackMappingClass` property (see `ROKRadio (ROKMapping)` interface for more informations)
+ */
 @interface ROKRadio (ROKRequest)
 
+// Returns a set of results
 - (void)lastTracks:(ROKRadioLastTracksBlock)completion;
+
+// Returns only one result
 - (void)lastTrack:(ROKRadioLastTrackBlock)completion;
 
 @end
 
+/**
+ *  This category describes how object mapping works for track results
+ *  If the `trackMappingClass` is set, the above request methods will returns objects of type of `trackMappingClass`
+ *  `trackMappingClass` type should conforms to `ROKTrack` protocol
+ */
 @interface ROKRadio (ROKMapping)
 
 @property (unsafe_unretained, nonatomic) Class<ROKTrack> trackMappingClass;
